0d8b2fb44a7eb1ecf704dcfe2a0e26f64ebec17b
Ilham Abdul Hakim <70672465+IlhamAbdul07@users.noreply.github.com>
2025-09-09 20:55:04 +0700

    initial commit

---
+++ lib/widgets/login_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginCard extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+  final bool isPasswordVisible;
+  final bool isLoading;
+  final VoidCallback onTogglePassword;
+  final VoidCallback onLogin;
+
+  const LoginCard({
+    super.key,
+    required this.formKey,
+    required this.usernameController,
+    required this.passwordController,
+    required this.isPasswordVisible,
+    required this.isLoading,
+    required this.onTogglePassword,
+    required this.onLogin,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/snackbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class General {
+  static Future<void> showSnackBar(
+    BuildContext context,
+    dynamic message, {
+    int? durationSeconds,
+  }) async {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: SizedBox(
+          width: 200, // Lebar maksimum Snackbar
+          child: Center(
+            child: Text(
+              message,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        duration: Duration(seconds: durationSeconds ?? 2),
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        backgroundColor: Colors.black87,
+      ),
+    );
+  }
+}
